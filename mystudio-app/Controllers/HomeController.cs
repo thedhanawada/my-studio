c60797a128cfcc32e1d89765e2d00dca17c3ebbf
ndhanawada <ndhanawada@student.unimelb.edu.au>
2020-03-22 02:21:03 +1100

    L&D 1

--- mystudio-app/Controllers/HomeController.cs
+++ mystudio-app/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace mystudio_app.Controllers
             return View();
         }
 
+        public IActionResult TestRun()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
